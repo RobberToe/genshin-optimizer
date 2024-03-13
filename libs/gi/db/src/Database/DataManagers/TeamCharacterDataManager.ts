674831987fdfa6f587f4dd720994ad636dbb258e
RobberToe <62998412+RobberToe@users.noreply.github.com>
2024-03-13 13:08:59 +0100

    Revert "Add a indicator that other characters are using the same artifacts/weapon in a teammates loadout (#1556)"
    This reverts commit 8a5d34fbff63b84dc8c2750263dedcb1f17cb22f.

--- libs/gi/db/src/Database/DataManagers/TeamCharacterDataManager.ts
+++ libs/gi/db/src/Database/DataManagers/TeamCharacterDataManager.ts
@@ -356,9 +356,6 @@ export class TeamCharacterDataManager extends DataManager<
     return defaultInitialWeapon()
   }
 
-  /**
-   * Note: this doesnt return any artifacts(all undefined) when the current teamchar is using a TC Build.
-   */
   getLoadoutArtifacts(
     teamCharId: string
   ): Record<ArtifactSlotKey, ICachedArtifact | undefined> {
@@ -378,6 +375,7 @@ export class TeamCharacterDataManager extends DataManager<
         if (!build) return objKeyMap(allArtifactSlotKeys, () => undefined)
         return objMap(build.artifactIds, (id) => this.database.arts.get(id))
       }
+      //TODO case 'TC'
     }
     return objKeyMap(allArtifactSlotKeys, () => undefined)
   }
