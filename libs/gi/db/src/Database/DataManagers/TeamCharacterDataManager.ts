442e7a621364db783c09ba31d2248729a4c44a7d
RobberToe <62998412+RobberToe@users.noreply.github.com>
2024-03-13 13:02:50 +0100

    Revert "update migration naming (#1658)"
    This reverts commit fdd23465fc1cbe7024328e124936223886f8d001.

--- libs/gi/db/src/Database/DataManagers/TeamCharacterDataManager.ts
+++ libs/gi/db/src/Database/DataManagers/TeamCharacterDataManager.ts
@@ -115,10 +115,10 @@ export class TeamCharacterDataManager extends DataManager<
       num <= existingUndercKey.length * 2;
       num++
     ) {
-      const name = `${characterKey} Loadout ${num}`
+      const name = `Loadout Name ${num}`
       if (existingUndercKey.some((tc) => tc.name !== name)) return name
     }
-    return `${characterKey} Loadout`
+    return `Loadout Name`
   }
   override validate(obj: unknown): TeamCharacter | undefined {
     const { key: characterKey } = obj as TeamCharacter
